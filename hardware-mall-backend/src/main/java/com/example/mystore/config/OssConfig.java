4b1f30345292dca49790abaedd06557b2566d613
xllcbc <xllcbc@github.com>
2026-07-11 22:05:04 +0800

    refactor: 移除冗余的 OssConfig，OssProperties 通过 @Component 自我激活

--- hardware-mall-backend/src/main/java/com/example/mystore/config/OssConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.mystore.config;
-
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableConfigurationProperties(OssProperties.class)
-public class OssConfig {
-}
